1135ec8e1bbb80824d7f386f34ce9d59aa089170
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-09-22 11:50:14 -0300

    teste

--- index.php
+++ index.php
@@ -6,55 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 1 </title>
 
-    <style>
-
-        *{box-sizing: border-box;}
-
-        body{
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            font-size: 18px;
-        }
-
-        h1, h2 {
-            text-align: center;
-        }
-
-        p a{
-            font-size: 25px;
-            margin: 10px;
-        }
-        
-        table{            
-            box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
-        }
-
-        table, td, tr {
-             border: 1px solid; 
-             border-collapse: collapse;  
-             /* margin: auto;  */
-             background-color: cornsilk          
-        }      
-        
-        caption{          
-              font-size: 30px;
-              padding: 5px;              
-        }
-
-        th{
-            background-color: #000000;
-            color: #fff;
-            padding: 10px;            
-        }
-
-        td{ 
-            padding: 10px 50px;               
-        }      
-
-        tr:hover{
-            background-color: cornsilk;
-            color: #000000;
-        }
-    </style>
+   
 
 </head>
 
--- src/Livro.php
+++ src/Livro.php
@@ -4,33 +4,7 @@ class Livro{
     private string $autor;
     private int $paginas = 0;
 
-    //Titulo
-    public function setTitulo(string $titulo):void{
-        $this->titulo = $titulo;
-    }
-
-    public function getTitulo():string{
-        return $this->titulo;
-    }
- 
-    //Autor
-    public function setAutor(string $autor):void{
-        $this->autor = $autor;
-    }
-
-    public function getAutor():string{
-        return $this->autor;
-    }
-
-    //Páginas
-    public function setPaginas(int $paginas):void{
-        $this->paginas = $paginas;
-    }
-
-    public function getPaginas():int{
-        return $this->paginas;
-    }
-
+    
 
 }
 
